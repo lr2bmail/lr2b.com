99781535bb3ff504c40fe706ad64a3eeaaa17122
lr2bmail <lr2b.com@gmail.com>
2026-06-14 10:18:02 +0200

    Improve project SEO and work page UX

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const menuButton = document.querySelector("[data-menu-button]");
 const mobileMenu = document.querySelector("[data-mobile-menu]");
-const year = document.querySelector("[data-year]");
-
-if (year) {
-  year.textContent = new Date().getFullYear();
-}
 
 if (menuButton && mobileMenu) {
   menuButton.addEventListener("click", () => {
